6c4dcd74da5d88d36b7826e278a798c3e95445c6
Felipe Toffetti <139416594+toffettl@users.noreply.github.com>
2025-01-18 18:36:19 -0300

    Corrigindo

--- Ex.py
+++ Ex.py
@@ -2,7 +2,7 @@ primeiroValor = input("Digite um valor: ")
 segundoValor = input("Digite outro: ")
 
 if primeiroValor < segundoValor:
-    print(f"{str(primeiroValor)} é menor que {str(segundoValor)}")
+    print(f"{str(segundoValor)} é maior que {str(primeiroValor)}")
 elif primeiroValor > segundoValor:
     print(f"{str(primeiroValor)} é maior que {str(segundoValor)}")
 else:
